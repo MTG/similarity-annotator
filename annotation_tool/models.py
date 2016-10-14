d829e5eea339a6e5085360b7b6ac06dd761dbe90
Oriol Romani <oriolromanipicas@gmail.com>
2016-10-14 16:08:00 +0200

    Add user in Dataset object

--- annotation_tool/models.py
+++ annotation_tool/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Tier(models.Model):
@@ -14,6 +15,7 @@ class Tier(models.Model):
 class Dataset(models.Model):
     name = models.CharField(max_length=50)
     tiers = models.ManyToManyField(Tier, blank=True, null=True)
+    users = models.ManyToManyField(User, blank=True, null=True)
 
     def __str__(self):
         return self.name
