894e0da3f42ee477f914df953c8ca757f6205ba0
Oriol Romani <oriolromanipicas@gmail.com>
2016-10-14 17:49:04 +0200

    Retrive list of sounds and datasets in view

--- annotation_tool/views.py
+++ annotation_tool/views.py
@@ -1,12 +1,18 @@
 from django.http import HttpResponse
+from models import Dataset
 
 
 def dataset_list(request):
-    return HttpResponse("The list of datasets")
+    list_of_datasets = Dataset.objects.all()
+    output = ','.join([dataset.name for dataset in list_of_datasets])
+    return HttpResponse("The list of datasets: %s" % output)
 
 
 def sound_list(request, dataset_id):
-    return HttpResponse("The list of sounds for the dataset %s" % dataset_id)
+    dataset = Dataset.objects.get(id=dataset_id)
+    list_of_sounds = dataset.sounds.all()
+    output = ','.join([sound.filename for sound in list_of_sounds])
+    return HttpResponse("List of sounds for the dataset %s: %s" % (dataset_id, output))
 
 
 def sound_detail(request, sound_id):
