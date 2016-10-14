2200192614f4ebaf7f5a16a91682f7d3e4339ac4
Oriol Romani <oriolromanipicas@gmail.com>
2016-10-14 15:49:17 +0200

    Move DB to local settings

--- Annotation/settings.py
+++ Annotation/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+try:
+    from local_settings import *
+except ImportError:
+    pass
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
