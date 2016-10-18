f727b6844ff604f72d5a0802e5f478ccfd999de3
Oriol Romani <oriolromanipicas@gmail.com>
2016-10-18 11:17:49 +0200

    Add annotation model

--- annotation_tool/models.py
+++ annotation_tool/models.py
@@ -14,8 +14,8 @@ class Tier(models.Model):
 
 class Dataset(models.Model):
     name = models.CharField(max_length=50)
-    tiers = models.ManyToManyField(Tier, blank=True, null=True)
-    users = models.ManyToManyField(User, blank=True, null=True)
+    tiers = models.ManyToManyField(Tier, blank=True)
+    users = models.ManyToManyField(User, blank=True)
 
     def __str__(self):
         return self.name
@@ -24,3 +24,10 @@ class Dataset(models.Model):
 class Sound(models.Model):
     filename = models.CharField(max_length=200)
     dataset = models.ForeignKey(Dataset, related_name='sounds')
+
+
+class Annotation(models.Model):
+    start_time = models.IntegerField("start_time")
+    end_time = models.IntegerField("end_time", blank=True, null=True)
+    sound = models.ForeignKey(Sound, related_name='annotations')
+    related_annotation = models.ForeignKey('self', blank=True, null=True)
