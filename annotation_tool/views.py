1f7c1dc71d08b9fdda9036d504c8c0b5db5952e5
Oriol Romani <oriolromanipicas@gmail.com>
2016-10-18 14:51:08 +0200

    Return Http404

--- annotation_tool/views.py
+++ annotation_tool/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from models import Dataset
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from models import Dataset, Sound
 
 
 def dataset_list(request):
@@ -10,11 +10,16 @@ def dataset_list(request):
 
 
 def sound_list(request, dataset_id):
-    dataset = Dataset.objects.get(id=dataset_id)
-    sounds_list = dataset.sounds.all()
-    context = {'sounds_list': sounds_list}
+    dataset = get_object_or_404(Dataset, id=dataset_id)
+    if dataset is Http404:
+        context = dataset
+    else:
+        sounds_list = dataset.sounds.all()
+        context = {'sounds_list': sounds_list}
     return render(request, 'annotation_tool/sounds_list.html', context)
 
 
 def sound_detail(request, dataset_id, sound_id):
-    return HttpResponse("This is the annotation page for sound %s from dataset %s" % (sound_id, dataset_id))
+    sound = get_object_or_404(Sound, id=sound_id)
+    context = {'sound': sound}
+    return render(request, 'annotation_tool/sound_detail.html', context)
