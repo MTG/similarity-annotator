02a148ad85d7721665f3f2543eaff0deef3cd9ce
Oriol Romani <oriolromanipicas@gmail.com>
2016-10-20 11:07:10 +0200

    Register annotation similarity in admin

--- annotation-app/admin.py
+++ annotation-app/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
-from .models import Sound, Tier, Exercise, Annotation
+from .models import Sound, Tier, Exercise, Annotation, AnnotationSimilarity
 
 admin.site.register(Sound)
 admin.site.register(Tier)
 admin.site.register(Exercise)
 admin.site.register(Annotation)
+admin.site.register(AnnotationSimilarity)
 
 
