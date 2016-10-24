997ed2082e1e7c883308cabd1a7c7385ebcab103
Oriol Romani <oriolromanipicas@gmail.com>
2016-10-24 11:55:45 +0200

    Fix urls

--- similarity-annotator/urls.py
+++ similarity-annotator/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^annotation-app/', include('annotation-app.urls')),
+    url(r'', include('annotation-app.urls')),
     url(r'^accounts/', include('accounts.urls')),
 ]
