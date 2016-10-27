1760582fb109c6c487a1fd1942eedd4fb81702a4
Andres Ferraro <andres.ferraro@upf.edu>
2016-10-27 19:20:28 +0200

    Add annotation method

--- annotation-app/urls.py
+++ annotation-app/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     url(r'^$', views.exercise_list, name='exercise_list'),
     url(r'^(?P<exercise_id>[0-9]+)/sound_list/$', views.sound_list, name='sound_list'),
     url(r'^(?P<exercise_id>[0-9]+)/sound_detail/(?P<sound_id>[0-9]+)$', views.sound_detail, name='sound_detail'),
+    url(r'^annotation_action/(?P<sound_id>[0-9]+)/(?P<tier_id>[0-9]+)$',
+        views.annotation_action, name='annotation-action'),
 ]
--- annotation-app/views.py
+++ annotation-app/views.py
@@ -1,7 +1,10 @@
+import json
+
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.http import Http404
-from .models import Exercise, Sound
+from django.http import Http404, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import Annotation, Exercise, Sound, Tier
 from django.conf import settings
 
 
@@ -29,4 +32,39 @@ def sound_detail(request, exercise_id, sound_id):
     context = {'sound': sound}
     return render(request, 'annotation-app/sound_detail.html', context)
 
+@login_required
+@csrf_exempt
+def annotation_action(request, sound_id, tier_id):
+    post_body = json.loads(request.body)
+    name = post_body.get('name', '')
+    start = post_body['startTime']
+    end = post_body['endTime']
+    action = post_body['action']
+    sound = get_object_or_404(Sound, id=sound_id)
+    tier = get_object_or_404(Tier, id=tier_id)
 
+    out = {'status': 'error'}
+    if action == 'add':
+        annotation = Annotation()
+        annotation.name = name
+        annotation.start_time = start
+        annotation.end_time = end
+        annotation.submitted_by = request.user
+        annotation.sound = sound
+        annotation.tier = tier
+        annotation.save()
+        out = {'status': 'success', 'annotation_id': annotation.id}
+    else:
+        annotation_id = post_body['annotation_id']
+        annotation = get_object_or_404(Annotation, id=annotation_id)
+        if action == 'remove':
+            annotation.remove()
+            out = {'status': 'success'}
+        elif action == 'edit':
+            annotation.name = name
+            annotation.start_time = start
+            annotation.end_time = end
+            annotation.submitted_by = request.user
+            annotation.save()
+            out = {'status': 'success', 'annotation_id': annotation.id}
+    return JsonResponse(out)
