824a727b4888b3c2ce3f0eb7adb0e4586c4eacd0
Oriol Romani <oriolromanipicas@gmail.com>
2016-12-09 15:32:49 +0100

    Create development settings

--- simannotator/settings.py
+++ simannotator/settings.py
@@ -133,3 +133,7 @@ TEMP_ROOT = "/tmp"
 STATIC_ROOT = "/static"
 
 GEARMAN_JOB_SERVERS = ["gearman:4730"]
+
+# in case some development settings are used
+if os.path.isfile(os.path.join(os.getcwd(), 'simannotator/development_settings.py')):
+    from .development_settings import *
