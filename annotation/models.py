ff337fd3620aba844375dc23c9432b33a38e0fd7
Oriol Romani <oriolromanipicas@gmail.com>
2016-12-21 13:19:15 +0100

    Add is_dicarded field in sound model

--- annotation/models.py
+++ annotation/models.py
@@ -31,6 +31,7 @@ class Sound(models.Model):
     filename = models.CharField(max_length=200)
     exercise = models.ForeignKey(Exercise, related_name='sounds')
     has_annotations = models.BooleanField(default=False)
+    is_discarded = models.BooleanField(default=False)
 
     def __str__(self):
         return self.filename
