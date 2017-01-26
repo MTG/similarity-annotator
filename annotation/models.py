2a4b7020edb77abab7812a0597f30dbb9caeb875
Oriol Romani <oriolromanipicas@gmail.com>
2017-01-26 13:37:48 +0100

    Tier attribute for point annotations. Related to #58

--- annotation/models.py
+++ annotation/models.py
@@ -32,6 +32,7 @@ class Tier(models.Model):
     parent_tier = models.ForeignKey('self', blank=True, null=True, related_name='child_tiers')
     exercise = models.ForeignKey(Exercise, related_name='tiers')
     entire_sound = models.BooleanField(default=False)
+    point_annotations = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
