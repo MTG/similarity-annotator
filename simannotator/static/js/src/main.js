18a97718e5cd8f8cc14e1e09677a03f07099cd7b
Andres Ferraro <andres.ferraro@upf.edu>
2017-02-01 20:55:38 +0100

    Reduce resolution of spectrogram
    issue #61

--- simannotator/static/js/src/main.js
+++ simannotator/static/js/src/main.js
@@ -40,7 +40,7 @@ function UrbanEars() {
     });
 
     // Create wavesurfer (audio visualization component)
-    var height = 128;
+    var height = 64;
     this.wavesurfer = Object.create(WaveSurfer);
     this.wavesurfer.init({
         container: '.audio_visual',
