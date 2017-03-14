c7a0b5a769000c419371546c93a0caa01722e6f7
Oriol Romani <oriolromanipicas@gmail.com>
2017-03-14 13:26:51 +0100

    Test sound models method to get annotations as a dict

--- annotation/tests/test_models.py
+++ annotation/tests/test_models.py
@@ -2,7 +2,7 @@
 from django.test import TestCase, Client
 
 from django.contrib.auth.models import User
-from annotation.models import DataSet, Exercise, Tier
+from annotation.models import DataSet, Exercise, Tier, Sound, Annotation
 
 
 class TierModelTests(TestCase):
@@ -65,3 +65,45 @@ class TierModelTests(TestCase):
         self.assertTrue(special_child_tier_1 in list_of_special_parent_tiers)
         self.assertTrue(self.tier_1 in list_of_special_parent_tiers)
 
+
+class SoundModelTests(TestCase):
+
+    def setUp(self):
+        data_set_name = 'test_data_set_2'
+        self.data_set = DataSet.objects.create(name=data_set_name)
+        exercise_name = 'test_exercise'
+        self.exercise = Exercise.objects.create(data_set=self.data_set, name=exercise_name)
+
+        username = 'test'
+        password = '1234567'
+        self.user = User.objects.create(username=username)
+        self.user.set_password(password)
+        self.user.save()
+
+        self.test_client = Client()
+        self.test_client.login(username=username, password=password)
+
+        self.reference_sound_2 = Sound.objects.create(filename='reference sound', exercise=self.exercise,
+                                                      original_filename='')
+        self.sound_2 = Sound.objects.create(filename='test sound', exercise=self.exercise, original_filename='')
+
+    def test_get_annotations_from_sound(self):
+        annotations = self.sound_2.get_annotations_as_dict()
+
+        self.assertTrue(isinstance(annotations, dict))
+        self.assertEqual(annotations, {})
+
+        # create tier for annotations
+        tier = Tier.objects.create(name='test tier', exercise=self.exercise)
+        # create annotations
+        start_time = 1
+        end_time = 2
+        annotation_1 = Annotation.objects.create(start_time=start_time, end_time=end_time, sound=self.sound_2,
+                                                 tier=tier, user=self.user)
+        annotations = self.sound_2.get_annotations_as_dict()
+
+        self.assertTrue(isinstance(annotations, dict))
+        self.assertTrue('test tier' in annotations.keys())
+        self.assertEqual(len(annotations['test tier']), 1)
+        self.assertEqual(annotations['test tier'][0]['start_time'], annotation_1.start_time)
+        self.assertEqual(annotations['test tier'][0]['end_time'], annotation_1.end_time)
